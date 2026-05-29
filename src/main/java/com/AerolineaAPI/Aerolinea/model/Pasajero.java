b4eb0b6410c6a810f4cc9b07238240e6331a39aa
mariagomez08 <mariiangelgomezsg@gmail.com>
2026-05-28 22:16:48 -0500

    Se agrega get y set de pasajero para id

--- src/main/java/com/AerolineaAPI/Aerolinea/model/Pasajero.java
+++ src/main/java/com/AerolineaAPI/Aerolinea/model/Pasajero.java
@@ -35,6 +35,14 @@ public class Pasajero {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNombre() {
         return nombre;
     }
